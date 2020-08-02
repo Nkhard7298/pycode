0c9c57a63e506f44f22ee4dd19445db8aaaf515a
Nkhard7298 <khardnaman@gmail.com>
2020-08-02 17:28:17 +0530

    Fixed: Sript

--- timeloc.py
+++ timeloc.py
@@ -5,24 +5,36 @@ from bs4 import BeautifulSoup
 
 def time(update,context):
 	city_name=update.message.text[6:]
-	r=requests.get("https://google.com/search?q="+city_name+"Time")
-	response=r.content
-	#print(response)
-	soup=BeautifulSoup(response,"html.parser")
-	rows=soup.find_all('div',{'class':'BNeawe iBp4i AP7Wnd'})
-	for row in rows:
-    		try:
-    			if row.find('div').text is None:
-    				update.message.reply_text(" ")
-    			else:
-    				update.message.reply_text(row.find('div').text)
-    		except:
-        		update.message.reply_text("Thank you!")
-        	# except:
-        	# 	update.message.reply_text("Something went wrong")
+	headers = {"User-Agent": "api"}
+	city = city_name.replace(' ', '+')
+	r = requests.get(f"https://www.google.com/search?q={city}+Time+Now", headers=headers)
+	b = BeautifulSoup(r.text, "html.parser")
+	t = b.findAll('div')
+	p=len(t[34].text)
+	if p < 10:
+		update.message.reply_text(t[34].text)
+	else:
+		update.message.reply_text("City Not Found")
 
 
 updater = Updater('1251053034:AAGMDVLqSk2VW6blNtqhG98hSt4mmnH9A1I', use_context=True)
 updater.dispatcher.add_handler(MessageHandler(Filters.text,time))
 updater.start_polling()
 updater.idle()
+
+
+# r=requests.get("https://google.com/search?q="+city_name+"current Time")
+# 	response=r.content
+# 	#print(response)
+# 	soup=BeautifulSoup(response,"html.parser")
+# 	rows=soup.find_all('div',{'class':'BNeawe iBp4i AP7Wnd'})
+# 	for row in rows:
+#     		try:
+#     			if row.find('div').text is None:
+#     				update.message.reply_text(" ")
+#     			else:
+#     				update.message.reply_text(row.find('div').text)
+#     		except:
+#         		update.message.reply_text("Thank you!")
+#         	# except:
+#         	# 	update.message.reply_text("Something went wrong")
